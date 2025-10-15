e6adc134cf73acc5cbe78c70f79f5eb41c809fda
Rouxist <name.of.squaner@gmail.com>
2025-10-15 21:46:07 +0900

    feat: added scripts for fine-tuning with updates on env setup and README

--- README.md
+++ README.md
@@ -4,9 +4,17 @@
 
 This repository contains the work for the 2025 graduation project at the Department of Data Science, Hanyang University.
 
+\*\*This work applies [ClipCap](https://github.com/rmokady/CLIP_prefix_caption) on histopathology domain.
+
+\*\*We used [Quilt1M](https://github.com/wisdomikezogwo/quilt1m) dataset to utilize large amount of histopathology image-text pair.
+
+\*\*It combines [Quilt-B-32](https://huggingface.co/wisdomik/QuiltNet-B-32) and [BioGPT](https://github.com/microsoft/BioGPT) to handle histopathology domain with ClipCap.
+
 This project was conducted by [Ammar Rais](https://www.linkedin.com/in/ammar-rais/) and [Yongjin Kang](https://www.linkedin.com/in/yongjin-kang/).
 
 ## Environment Setup
+
+\*\* This environment covers libraries required for `parse_image.py` and `train.py'. Other scripts may require additional libraries.
 
 ```
 git clone https://github.com/Rouxist/graduation-project-2025
@@ -22,6 +30,18 @@ In the original dataset, some images are nearly identical with slight difference
 The output CSV file from `preprocess_dedup_same_images.py` is further processed by `preprocess_json_converter.py`, which separates the multiple captions of each image using the pandas .explode() function. It generates final .json file, which is later used by `parse_image.py`.
 
 ## Training
+
+\*\*First, encode images using `parse_image.py`.
+
+```
+python parse_image.py
+```
+
+\*\*Then, fine-tune like this.
+
+```
+python train.py --data_train ./Data/quilt1m/oscar_split_train.pkl --out_dir ./quilt1m_train/
+```
 
 ## Examples
 
---
+++ train.py
@@ -0,0 +1,202 @@
+import os
+import sys
+from typing import Tuple, Optional, Union
+from enum import Enum
+import argparse
+import json
+from tqdm import tqdm
+import pickle
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as nnf
+from torch.utils.data import Dataset, DataLoader
+
+from transformers import BioGptTokenizer, BioGptForCausalLM, AdamW, get_linear_schedule_with_warmup
+from model import *
+import time
+
+
+class MappingType(Enum):
+    MLP = 'mlp'
+    Transformer = 'transformer'
+
+
+class ClipCapDataset(Dataset):
+
+    def __len__(self) -> int:
+        return len(self.captions_tokens)
+
+    def pad_tokens(self, item: int):
+        tokens = self.captions_tokens[item]
+        padding = self.max_seq_len - tokens.shape[0]
+        if padding > 0:
+            tokens = torch.cat((tokens, torch.zeros(padding, dtype=torch.int64) - 1))
+            self.captions_tokens[item] = tokens
+        elif padding < 0:
+            tokens = tokens[:self.max_seq_len]
+            self.captions_tokens[item] = tokens
+        mask = tokens.ge(0)  # mask is zero where we out of sequence
+        tokens[~mask] = 0
+        mask = mask.float()
+        mask = torch.cat((torch.ones(self.prefix_length), mask), dim=0)  # adding prefix mask
+        return tokens, mask
+
+    def __getitem__(self, item: int) -> Tuple[torch.Tensor, ...]:
+        tokens, mask = self.pad_tokens(item)
+        prefix = self.prefixes[self.caption2embedding[item]]
+        if self.normalize_prefix:
+            prefix = prefix.float()
+            prefix = prefix / prefix.norm(2, -1)
+        return tokens, mask, prefix
+
+    def __init__(self, data_path: str,  prefix_length: int, normalize_prefix=False):
+        self.tokenizer = BioGptTokenizer.from_pretrained("microsoft/biogpt")
+        self.prefix_length = prefix_length # 10, by default
+        self.normalize_prefix = normalize_prefix # False, by default
+        with open(data_path, 'rb') as f:
+            all_data = pickle.load(f)
+        print("Data size is %0d" % len(all_data["clip_embedding"]))
+        sys.stdout.flush()
+        self.prefixes = all_data["clip_embedding"]
+        captions_raw = all_data["captions"]
+        self.image_ids = [caption["image_id"] for caption in captions_raw]
+        self.captions = [caption['caption'] for caption in captions_raw]
+        if os.path.isfile(f"{data_path[:-4]}_tokens.pkl"):
+            with open(f"{data_path[:-4]}_tokens.pkl", 'rb') as f:
+                self.captions_tokens, self.caption2embedding, self.max_seq_len = pickle.load(f)
+        else:
+            self.captions_tokens = []
+            self.caption2embedding = []
+            max_seq_len = 0
+            for caption in captions_raw:
+                self.captions_tokens.append(torch.tensor(self.tokenizer.encode(caption['caption']), dtype=torch.int64))
+                self.caption2embedding.append(caption["clip_embedding"])
+                max_seq_len = max(max_seq_len, self.captions_tokens[-1].shape[0])
+            with open(f"{data_path[:-4]}_tokens.pkl", 'wb') as f:
+                pickle.dump([self.captions_tokens, self.caption2embedding, max_seq_len], f)
+        all_len = torch.tensor([len(self.captions_tokens[i]) for i in range(len(self))]).float()
+        self.max_seq_len = min(int(all_len.mean() + all_len.std() * 10), int(all_len.max()))
+
+class HistoClipCap(nn.Module):
+    def __init__(self, prefix_length: int, clip_length: Optional[int] = None, prefix_size: int = 512,
+                 num_layers: int = 8, mapping_type: MappingType = MappingType.MLP):
+        super(HistoClipCap, self).__init__()
+        self.prefix_length = prefix_length
+        self.gpt = BioGptForCausalLM.from_pretrained("microsoft/biogpt")
+        self.gpt_embedding_size = self.gpt.biogpt.embed_tokens.weight.shape[1] # replaced 'transformer.wte' of GPT2 with 'biogpt.embed_tokens' of BioGPT
+        if mapping_type == MappingType.MLP:
+            self.clip_project = MLP((prefix_size, (self.gpt_embedding_size * prefix_length) // 2,
+                                     self.gpt_embedding_size * prefix_length))
+        else:
+            self.clip_project = TransformerMapper(prefix_size, self.gpt_embedding_size, prefix_length,
+                                                                     clip_length, num_layers)
+
+    def get_dummy_token(self, batch_size: int, device: torch.device) -> torch.Tensor:
+        return torch.zeros(batch_size, self.prefix_length, dtype=torch.int64, device=device)
+
+    def forward(self, tokens: torch.Tensor, prefix: torch.Tensor, mask: Optional[torch.Tensor] = None,
+                labels: Optional[torch.Tensor] = None):
+        embedding_text = self.gpt.biogpt.embed_tokens(tokens) # replaced 'wte'/'transformer' of GPT2 with 'embed_tokens'/'biogpt' of BioGPT
+        prefix_projections = self.clip_project(prefix).view(-1, self.prefix_length, self.gpt_embedding_size)
+        embedding_cat = torch.cat((prefix_projections, embedding_text), dim=1)
+        if labels is not None:
+            dummy_token = self.get_dummy_token(tokens.shape[0], tokens.device)
+            labels = torch.cat((dummy_token, tokens), dim=1)
+        out = self.gpt(inputs_embeds=embedding_cat, labels=labels, attention_mask=mask)
+        return out
+
+
+def train(dataset: ClipCapDataset, model: HistoClipCap, args,
+          lr: float = 2e-5, warmup_steps: int = 5000, output_dir: str = ".", output_prefix: str = ""):
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"\nUsing device: {device}")
+    print(f"batch_size: {args.bs}      epochs: {args.epochs}")
+
+    batch_size = args.bs
+    epochs = args.epochs
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    model = model.to(device)
+    model.train()
+    optimizer = AdamW(model.parameters(), lr=lr)
+    train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=True)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=warmup_steps, num_training_steps=epochs * len(train_dataloader)
+    )
+    # save_config(args)
+    for epoch in range(epochs):
+        print(f">>> Training epoch {epoch+1}")
+        sys.stdout.flush()
+        progress = tqdm(total=len(train_dataloader), desc=output_prefix)
+
+        for idx, (tokens, mask, prefix) in enumerate(train_dataloader):
+            model.zero_grad()
+            tokens, mask, prefix = tokens.to(device), mask.to(device), prefix.to(device, dtype=torch.float32)
+            outputs = model(tokens, prefix, mask)
+            logits = outputs.logits[:, dataset.prefix_length - 1: -1]
+            loss = nnf.cross_entropy(logits.reshape(-1, logits.shape[-1]), tokens.flatten(), ignore_index=0)
+
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+            optimizer.zero_grad()
+            progress.set_postfix({"loss": loss.item()})
+            progress.update(1)
+            if (idx + 1) % 10000 == 0:
+                torch.save(
+                    model.state_dict(),
+                    os.path.join(output_dir, f"{output_prefix}_latest.pt"),
+                )
+        progress.close()
+
+        if epoch % args.save_every == 0 or epoch == epochs - 1:
+            torch.save(
+                model.state_dict(),
+                os.path.join(output_dir, f"{output_prefix}-{epoch:03d}.pt"),
+            )
+    return model
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_train', default='./data/quilt1m/oscar_split_train.pkl')
+    parser.add_argument('--out_dir', default='./checkpoints')
+    parser.add_argument('--prefix', default='histo_clipcap', help='prefix for saved filenames')
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--save_every', type=int, default=1)
+    parser.add_argument('--prefix_length', type=int, default=10)
+    parser.add_argument('--prefix_length_clip', type=int, default=10)
+    parser.add_argument('--bs', type=int, default=40)
+    parser.add_argument('--only_prefix', dest='only_prefix', action='store_true')
+    parser.add_argument('--mapping_type', type=str, default='mlp', help='mlp/transformer')
+    parser.add_argument('--num_layers', type=int, default=8)
+    parser.add_argument('--is_rn', dest='is_rn', action='store_true')
+    parser.add_argument('--normalize_prefix', dest='normalize_prefix', action='store_true')
+    args = parser.parse_args()
+    prefix_length = args.prefix_length
+    dataset = ClipCapDataset(args.data_train, prefix_length, normalize_prefix=args.normalize_prefix)
+
+    prefix_dim = 640 if args.is_rn else 512
+    args.mapping_type = {'mlp': MappingType.MLP, 'transformer': MappingType.Transformer}[args.mapping_type]
+
+    if args.only_prefix:
+        print("Not supported yet")
+        # model = ClipCaptionPrefix(prefix_length, clip_length=args.prefix_length_clip, prefix_size=prefix_dim,
+        #                           num_layers=args.num_layers, mapping_type=args.mapping_type)
+        # print("Train only prefix")
+    else:
+        model = HistoClipCap(prefix_length, clip_length=args.prefix_length_clip, prefix_size=prefix_dim,
+                                  num_layers=args.num_layers, mapping_type=args.mapping_type)
+        print("Train both prefix and GPT")
+        sys.stdout.flush()
+    
+    start_time = time.time()
+    train(dataset, model, args, output_dir=args.out_dir, output_prefix=args.prefix)
+    
+    print(f"\n\nTook {start_time-time.time()} seconds")
+
+
+if __name__ == '__main__':
+    main()
