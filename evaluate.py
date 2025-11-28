607e918cf10e0471a8dd07624359ad39550cd320
Rouxist <name.of.squaner@gmail.com>
2025-11-28 17:37:58 +0900

    feat: added evaluation script

---
+++ evaluate.py
@@ -0,0 +1,30 @@
+import argparse
+from pycocotools.coco import COCO
+from pycocoevalcap.eval import COCOEvalCap
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ground_truth_dir', default='./evaluation/quilt_1m_test_example_ground_truth.json')
+    parser.add_argument('--pred_dir', default='./evaluation/quilt_1m_test_example_generated.json')
+    args = parser.parse_args()
+
+    annotation_file = args.ground_truth_dir
+    results_file = args.pred_dir
+
+    coco = COCO(annotation_file)
+    coco_result = coco.loadRes(results_file)
+
+    coco_eval = COCOEvalCap(coco, coco_result)
+
+    coco_eval.params['image_id'] = coco_result.getImgIds()
+
+    coco_eval.evaluate()
+
+    # print output evaluation scores
+    for metric, score in coco_eval.eval.items():
+        print(f'{metric}: {score:.3f}')
+
+    print("Evaluation is done.")
+
+if __name__ == '__main__':
+    main()
